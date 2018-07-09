9992af1c2463b2def1a1b20e5be3ae91a331b2fe
Be-a-laity <king.jxy@qq.com>
2018-07-09 16:47:06 +0800

    小修改

--- js/list.js
+++ js/list.js
@@ -1,3 +1,13 @@
 $(function(){
     lxfEndtime();
 });
+
+//让导航条固定在顶部
+// ;(function($){
+//     $(function(ele,){
+//         window.scroll(function(){
+//             if($(".ele").scroll());
+//             console.log($(".ele").scroll());
+//         })
+//     }(count-down))
+// }(window))
