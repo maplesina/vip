77bc6c698f47682661061d73cfcce10682c68a11
1373667568@qq.com <1373667568@qq.com>
2018-07-12 18:32:18 +0800

    首页电梯导航完善

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-;
-(function($) {
+;(function($) {
 	$(function() {
 		var $nav = $(".top-menu"); //得到导航对象
 		var $win = $(window); //得到窗口对象
@@ -18,3 +17,15 @@
 
 
 })(jQuery);
+
+//电梯导航
+;(function($){
+	$(function () {
+		LiftEffect({
+			"control1": ".lift-nav", 						  //侧栏电梯的容器
+			"control2": ".lift",                           //需要遍历的电梯的父元素
+			"target": [".t1", ".t2", ".t3", ".t4", ".t5", ".t6", ".t7", ".t8", ".t9", ".t10", ".t11"], //监听的内容，注意一定要从小到大输入
+			"current": "current" 						  //选中的样式
+		});
+	})
+})(jQuery)
