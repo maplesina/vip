11b260733c24dc36c5d34e032d70f3a8ecaa677c
Be-a-laity <king.jxy@qq.com>
2018-07-12 20:51:34 +0800

    购物袋完整版

--- js/list.js
+++ js/list.js
@@ -12,38 +12,7 @@ $(function(){
 //     }(count-down))
 // }(window))
 
-// 购物袋
-$(".side .shop").on("mouseenter",function(){
-    $(".gouwu").show();
-})
-//当鼠标进入shop的时候gouwu出现，当鼠标离开的时候gouwu隐藏
-$(".side .shop").on("mouseleave",function(){
-    $(".gouwu").hide();
-})
-$(".side .gouwu").on("mouseenter",function(){
-    $(".gouwu").show();
-})
-$(".side .gouwu").on("mouseleave",function(){
-    $(".gouwu").hide();
-})
-$(".login").on("mouseenter",function(){
-    $(this).parents(".user").css("backgroundColor","#333")
-})
-// 当浮上右边的图标的时候让他对应的p出来
-$(".side i").on("mouseenter",function(){
-    $(this).children().css("display","block")
-    $(this).children().css("right","40px")
-})
-$(".side  i").on("mouseleave",function(){
-    $(this).children().css("display","none")
-    $(this).children().css("right","-60px")
-})
-$(".side .shop").on("click",function(){
-    $(this).children(".head-portrait").css("display","block")
-})
-$(".side .shop").on("mouseleave",function(){
-    $(this).children(".head-portrait").css("display","none")
-})
+
 
 
 //悬浮的红包小窗口的时候显示和隐藏
@@ -76,9 +45,4 @@ $(function(){
     })
 })
 
-//回到顶部
-$('#top').click(function () {
-    $('html,body').animate({ 
-        scrollTop: 0 
-    }, 500);
-});
+
