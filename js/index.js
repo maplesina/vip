56241b89b11c8f39d4cbf9ba451cbe05841c680f
John Doe <johndoe@example.com>
2018-07-13 16:17:56 +0800

    qqqq

--- js/index.js
+++ js/index.js
@@ -28,4 +28,53 @@
 			"current": "current" 						  //选中的样式
 		});
 	})
+})(jQuery);
+
+//首页上的banner渐现轮播
+;(function($){
+	$(function() {
+				var n = 0;
+				$(".banner-pic").eq(n).css("opacity", 1)//初始化第一张显示
+				$(".banner-list-item").eq(n).addClass("active");
+				function autoplay() {
+					$(".banner-list-item").on("click",function  () {
+					var index=$(this).index();
+					n=index;
+					$(".banner-pic").eq(index).animate({
+							opacity: 1
+						}, 200)
+						$(".banner-list-item").eq(index).addClass("active");
+						$(".banner-pic").eq(index).siblings().animate({
+							opacity: 0
+						}, 200)
+						$(".banner-list-item").eq(index).siblings().removeClass("active");
+				});	
+				if (n==2) {
+					n=-1;
+				}					
+					timer = setInterval(function() {
+						n++;
+						$(".banner-pic").eq(n).siblings().animate({
+							opacity: 0
+						}, 300)
+						$(".banner-list-item").eq(n).siblings().removeClass("active");
+						$(".banner-pic").eq(n).animate({
+							opacity: 1
+						}, 300)
+						$(".banner-list-item").eq(n).addClass("active");
+						if(n == 2) {
+							n = -1;
+						}
+					}, 3000)
+					
+				};
+				autoplay();
+				$(".banner").on("mouseenter", function() {
+					clearInterval(timer);
+				});
+				$(".banner").on("mouseleave",
+					function() {
+						autoplay();
+					});
+			})
 })(jQuery)
