24797927a16cb99530bfb09d9a7319da5cac1720
John Doe <johndoe@example.com>
2018-07-20 17:23:32 +0800

    qqqqqqq

--- js/order.js
+++ js/order.js
@@ -1,13 +1,48 @@
-;(function($){
-    var divs=$(".tab-rig>div");
-    console.log(divs)
-    var lis=$(".center-subnav li");
-    console.log(lis)
-    var len=lis.length;
-    
-    $(lis).on("click",function(){
-        
-            // 获取点击li对应索引的div    将其显示出来    将除它之外的所有div隐藏
-            $(divs).eq($(this).index()).css("display","block").siblings("div").css("display","none");
-        })    
+;
+(function($) {
+	var divs = $(".tab-rig>div");
+	var lis = $(".center-subnav li");
+	var len = lis.length;
+
+	$(lis).on("click", function() {
+
+		// 获取点击li对应索引的div    将其显示出来    将除它之外的所有div隐藏
+		$(divs).eq($(this).index()).css("display", "block").siblings("div").css("display", "none");
+	})
+	//点击加减号的实现
+	var n = 0,
+	m=0,
+	j=0;
+	$(".list-item1").on("click", function() {
+		n = n + 1;
+		if(n % 2) {
+			$(this).css("background-position-y", "-33px");
+			$(this).parent().siblings().css("display","none");
+		} else {
+			$(this).css("background-position-y", "-49px")
+			$(this).parent().siblings().css("display","block");
+		}
+	})
+	$(".list-item2").on("click", function() {
+		m= m + 1;
+		if(m % 2) {
+			$(this).css("background-position-y", "-33px");
+			$(this).parent().siblings().css("display","none");
+		} else {
+			$(this).css("background-position-y", "-49px")
+			$(this).parent().siblings().css("display","block");
+		}
+	})
+	$(".list-item3").on("click", function() {
+		j= j + 1;
+		if(j % 2) {
+			$(this).css("background-position-y", "-33px");
+			$(this).parent().siblings().css("display","none");
+		} else {
+			$(this).css("background-position-y", "-49px")
+			$(this).parent().siblings().css("display","block");
+		}
+	})
 }(jQuery));
+
+
